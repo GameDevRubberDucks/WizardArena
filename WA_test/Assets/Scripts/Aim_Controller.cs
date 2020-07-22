64c1a5c0e44b2258c3145e6b7dbacbbbe9d5be3f
Bo <Wen.yu@ontariotechu.net>
2020-07-22 13:52:52 -0400

    [ADD] new branch setup

---
+++ WA_test/Assets/Scripts/Aim_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Aim_Controller : MonoBehaviour
+{
+
+
+    public bool cone;
+    public bool projectile;
+    public bool beam;
+    public bool aoe;
+    public Sprite sprite;
+    public float maxDistanceFromBody;
+    public float minDistanceFromBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ WA_test/Assets/Scripts/Aoe_AimController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aoe_AimController : MonoBehaviour
+{
+    public Sprite sprite;
+    public float maxDistanceFromBody = 1;
+    public float minDistanceFromBody = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
