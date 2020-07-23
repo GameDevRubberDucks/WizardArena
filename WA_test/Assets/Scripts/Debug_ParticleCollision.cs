80c20e55a92ce34a6e5fc4a45ebe4551db2e03ae
Dan <dmaccormick@hotmail.com>
2020-07-23 14:54:47 -0400

    Updated the meteor spell so that it works with collision, auto-destroy
    - Added a collider that appears for one frame that is set to send collision messages
    > Added a debug script that can be added to objects to determine if it is working
    - Added a script that automatically removes a spell after is finished
    > Works by checking to see when the particle system is totally complete
    > Needs to be placed on the parent of the prefab system

---
+++ WA_test/Assets/Scripts/Debug_ParticleCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Debug_ParticleCollision : MonoBehaviour
+{
+    // Notes:
+    // - For this function to be called, there are a couple of things to keep in mind
+    // - This object NEEDS a collider of some kind
+    // - This object DOES NOT need a rigidbody
+    // - The collision particle system in the FX NEEDS to have the "Collider" module enabled
+    //      > The collision particle system NEEDS to be set to World / 3D collision
+    //      > The collision particle system NEEDS to have "Send Collision Messages" enabled
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("OnParticleCollision() with " + other.gameObject.name);
+    }
+}
