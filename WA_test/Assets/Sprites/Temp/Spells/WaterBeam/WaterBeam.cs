545b633277ed7073fba35a34f672495d2c684a5c
Kody Wood <kodymrwood14@gmail.com>
2020-07-27 15:06:05 -0400

    [ADD] Water Beam and Icicle
    Both need work and I think the Water beam will need to be changed

---
+++ WA_test/Assets/Sprites/Temp/Spells/WaterBeam/WaterBeam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBeam : MonoBehaviour
+{
+    float scrollSpeed = -5.0f;
+    public Renderer rend;
+    //public LineRenderer lineRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //lineRend = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
